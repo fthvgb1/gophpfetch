66a9c221bc54d62589a6694ed14877434dbfa1f6
xing <fthvgb1@163.com>
2026-07-06 12:00:17 +0800

    optimize code

--- cmd/main.go
+++ cmd/main.go
@@ -11,73 +11,72 @@ import (
 
 func main() {
 	var file, jsonContent string
-	var input, single bool
+	var input, single, rr bool
 	var c int
 	flag.StringVar(&file, "f", "", "json file path")
 	flag.StringVar(&jsonContent, "j", "", "json content")
 	flag.BoolVar(&input, "i", false, "use stdin input json content")
 	flag.Int("c", 1, "concurrence number")
 	flag.BoolVar(&single, "s", false, "single request")
+	flag.BoolVar(&rr, "r", false, "directly output result not to json")
 	flag.Parse()
 	if file == "" && jsonContent == "" && !input && len(os.Args) < 2 {
 		fmt.Println("invalid parameter")
 		return
 	}
 	var requests any
-	if single {
-		requests = any(fetch.RequestItem{})
-	} else {
-		requests = any([]fetch.RequestItem{})
+	bytesFn := func(bytes []byte) (err error) {
+		if single {
+			var a fetch.RequestItem
+			err = json.Unmarshal(bytes, &a)
+			requests = a
+			return
+		}
+		var a []fetch.RequestItem
+		err = json.Unmarshal(bytes, &a)
+		requests = a
+		return
 	}
+	fileFn := func(path string) error {
+		bytes, err := os.ReadFile(file)
+		if err != nil {
+			return err
+		}
+		return bytesFn(bytes)
+	}
+	var err error
 	if file != "" {
-		bytes, err := os.ReadFile(file)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		err = json.Unmarshal(bytes, &requests)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+		err = fileFn(file)
 	} else if jsonContent != "" {
-		err := json.Unmarshal([]byte(jsonContent), &requests)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+		err = bytesFn([]byte(jsonContent))
 	} else if input {
-		bytes, err := io.ReadAll(os.Stdin)
-		if err != nil {
-			fmt.Println(err)
+		bytes, er := io.ReadAll(os.Stdin)
+		if er != nil {
+			err = er
 			return
 		}
-		err = json.Unmarshal(bytes, &requests)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+		err = bytesFn(bytes)
 	} else {
-		file = os.Args[1]
-		bytes, err := os.ReadFile(file)
-		if err != nil {
-			jsonContent = file
-			err = json.Unmarshal([]byte(jsonContent), &requests)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		} else {
-			err = json.Unmarshal(bytes, &requests)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		}
+		file = os.Args[len(os.Args)-1]
+		_ = fileFn(file)
+		err = bytesFn([]byte(file))
+	}
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 	if single {
 		r, _ := fetch.Request(requests.(fetch.RequestItem))
-		fmt.Print(r)
+		if rr {
+			fmt.Print(r.Result)
+			return
+		}
+		bytes, err := json.Marshal(r)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Print(string(bytes))
 		return
 	}
 	r, err := fetch.ExecuteRequests(requests.([]fetch.RequestItem), c)
@@ -85,5 +84,16 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Print(r)
+	if rr {
+		for _, item := range r {
+			fmt.Println(item.Result)
+		}
+		return
+	}
+	bytes, err := json.Marshal(r)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Print(string(bytes))
 }
